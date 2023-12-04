ac2f70689e422cbaca97ecbfd66c8f47448d9f1f
Karl Olaf Kuldmaa <karlolaf@dolmit.com>
2023-12-04 14:10:57 +0200

    add min with 50px to table and closing the application window

--- backend/src/main/java/ee/ut/euim/SpringAngularDemoApplication.java
+++ backend/src/main/java/ee/ut/euim/SpringAngularDemoApplication.java
@@ -3,19 +3,43 @@ package ee.ut.euim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 @SpringBootApplication
 public class SpringAngularDemoApplication {
 
 	public static void main(String[] args) throws IOException {
-		SpringApplication.run(SpringAngularDemoApplication.class, args);
 		Runtime rt = Runtime.getRuntime();
 		String url = "http://localhost:8080";
 		rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
-		System.out.println("Press 'Enter' key to exit.");
-		System.console().readLine();
-
+		SwingUtilities.invokeLater(
+			() -> {
+				createAndShowGUI();
+				SpringApplication.run(SpringAngularDemoApplication.class, args);
+			}
+		);
 	}
 
+	private static void createAndShowGUI() {
+		JFrame frame = new JFrame("Click the window to exit application");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
+
+
+		JButton exitButton = new JButton("Exit Application");
+		frame.add(exitButton);
+
+		exitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+
+		frame.setVisible(true);
+	}
 }
