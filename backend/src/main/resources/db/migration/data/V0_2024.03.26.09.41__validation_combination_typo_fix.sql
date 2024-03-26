d55fb20ed4973604770e75f104c47d218ea640aa
Karl Olaf Kuldmaa <karlolaf@dolmit.com>
2024-03-26 09:44:46 +0200

    fix typos in validation combination results

---
+++ backend/src/main/resources/db/migration/data/V0_2024.03.26.09.41__validation_combination_typo_fix.sql
@@ -0,0 +1,18 @@
+UPDATE validation_combination_result
+SET result_en='There is a different stakeholder whose action outcome in a slightly different context is incorrect. However, we don''t know whether'
+WHERE id=39;
+UPDATE validation_combination_result
+SET result_en='There is a stakeholder whose action outcome in a slightly different context is incorrect. However, we don''t know whether'
+WHERE id=40;
+UPDATE validation_combination_result
+SET result_en='There is a different stakeholder whose action outcome in a different context is incorrect. However, we don''t know whether'
+WHERE id=43;
+UPDATE validation_combination_result
+SET result_en='There is a stakeholder whose action outcome in a different context is incorrect. However, we don''t know whether'
+WHERE id=44;
+UPDATE validation_combination_result
+SET result_en='There is a different stakeholder whose action outcome in an unknown context is incorrect. However, we don''t know whether'
+WHERE id=47;
+UPDATE validation_combination_result
+SET result_en='There is a stakeholder whose action outcome in an unknown context is incorrect. However, we don''t know whether'
+WHERE id=48;
