4ed62f2f39313b40c0efb144179083aeddf8d4b4
Iris Kreinin <iris.kreinin@ut.ee>
2024-03-29 00:58:58 +0200

    Added local storage for answer selection

--- frontend/src/app/select/select.component.ts
+++ frontend/src/app/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, HostListener, ElementRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, ElementRef, OnInit } from '@angular/core';
 import { ValidationValue, ValidationValue2LabelMapping } from '../validation/model/validation-value';
 import { TranslateService } from '@ngx-translate/core';
 import { GlobalConstants } from '../constants/global-constants';
@@ -8,18 +8,28 @@ import { GlobalConstants } from '../constants/global-constants';
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css']
 })
-export class SelectComponent {
+export class SelectComponent implements OnInit {
   validationValue2LabelMapping = ValidationValue2LabelMapping;
   validationValues = Object.values(ValidationValue);
   selectedValue: ValidationValue;
   isToggled: boolean = false;
+  storageKey: string;
 
+  @Input() instanceId: string; // Unique identifier for each instance
   @Output() selectionChange: EventEmitter<ValidationValue> = new EventEmitter<ValidationValue>();
 
   constructor(
     private elementRef: ElementRef,
     private translateService: TranslateService
   ) {}
+
+  ngOnInit() {
+    this.storageKey = `selectedValue_${this.instanceId}`;
+    const storedValue = localStorage.getItem(this.storageKey);
+    if (storedValue) {
+      this.selectedValue = JSON.parse(storedValue);
+    }
+  }
 
   toggleSelect(): void {
     this.isToggled = !this.isToggled;
@@ -37,6 +47,7 @@ export class SelectComponent {
 
   onValueChange(validationValue: ValidationValue): void {
     this.selectedValue = validationValue;
+    localStorage.setItem(this.storageKey, JSON.stringify(validationValue));
     this.selectionChange.emit(validationValue)
     this.toggleSelect();
   }
